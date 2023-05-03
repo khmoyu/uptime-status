45dc5fb56c71fcc0fa1db6dd7b21a9e35b5a2b91
墨宇 <42474253+khmoyu@users.noreply.github.com>
2023-05-03 22:48:21 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,12 +2,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: '云生站点监测',
+  SiteName: '墨宇站点监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur1078385-b9802a0868414b8a975d5038',
+    'ur1360263-c27b61a773e8d8a7f8094873',
   ],
 
     // 日志天数
@@ -22,15 +22,15 @@ window.Config = {
   Navi: [
     {
       text: '主页',
-      url: 'https://tjys.tk'
+      url: 'https://uptime.khmoyu.cn'
     },
     {
       text: '博客',
-      url: 'https://blog.qikaile.tk'
+      url: 'https://khmoyu.cn'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/qikaile/uptime-status'
+      url: 'https://github.com/khmoyu/uptime'
     },
   ],
 };
